58bfd94d63c5cb1ef0fe57044488e1eefd932e0b
Callum Carmicheal <callumcarmicheal@gmail.com>
2019-04-29 20:12:11 +0100

    Removed test app, Created jar with all deps.
    + Finalized project.
    + Created jar file using maven shade to contain all 3rd party libraries so the project will work anywhere
    + Updated WFrame to v0.2.2-SNAPSHOT
    + Removed CSS file and inlined into site.html
    
    The program will now run with only the jar and nothing else required.

--- WFramework/src/main/java/com/callumcarmicheal/wframe/exception/RequestPathConfliction.java
+++ WFramework/src/main/java/com/callumcarmicheal/wframe/exception/RequestPathConfliction.java
@@ -19,11 +19,23 @@ public class RequestPathConfliction extends Exception {
      * @param conflictingMethod2 The second method that is conflicting
      */
     public RequestPathConfliction(String requestType, String requestPath, Method conflictingMethod1, Method conflictingMethod2) {
-        super("Conflicting web request for " + requestType + " " + requestPath + ".");
+        super("Conflicting web request for " + requestType + " " + requestPath + " " + 
+            " @ " + Package(conflictingMethod1.toGenericString()) + " and " + Package(conflictingMethod2.toGenericString()));
 
         this.requestPath = requestPath;
         this.requestType = requestType;
         this.conflictingMethod1 = conflictingMethod1;
         this.conflictingMethod2 = conflictingMethod2;
+    }
+
+
+    /**
+     * Generate a compressed abbreviated package name from a long package.
+     * 
+     * @param Package The package to abbreviate
+     * @return An abbreviated package
+     */
+    static String Package(String Package) {
+        return Package.replaceAll("\\B\\w+(\\.[a-z])", "$1");
     }
 }
--- WFramework/src/main/java/com/callumcarmicheal/wframe/testing/SimpleDebugApp.java
+++ WFramework/src/main/java/com/callumcarmicheal/wframe/testing/SimpleDebugApp.java
@@ -9,24 +9,24 @@ import com.callumcarmicheal.wframe.Server;
 import com.callumcarmicheal.wframe.web.Session;
 
 public class SimpleDebugApp {
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(9000, "com.callumcarmicheal.wframe.testing");
-        server.start();
-    }
+    // public static void main(String[] args) throws Exception {
+    //     Server server = new Server(9000, "com.callumcarmicheal.wframe.testing");
+    //     server.start();
+    // }
 
-    public SimpleDebugApp() {}
+    // public SimpleDebugApp() {}
 
-    @GetRequest("/")
-    public void getIndexPage(HttpRequest request) throws IOException {
-        Map<String,String> cookies = request.getRequestCookies();
-        Session s = request.session();
+    // @GetRequest("/")
+    // public void getIndexPage(HttpRequest request) throws IOException {
+    //     Map<String,String> cookies = request.getRequestCookies();
+    //     Session s = request.session();
 
-        request.Write("<pre>");
-        for (Map.Entry<String,String> cookie : cookies.entrySet())
-            request.Write(cookie.getKey() + " = " + cookie.getValue() + "\n");
+    //     request.Write("<pre>");
+    //     for (Map.Entry<String,String> cookie : cookies.entrySet())
+    //         request.Write(cookie.getKey() + " = " + cookie.getValue() + "\n");
 
-        request.Write("\n");
-        request.Send("Session Id = " + request.session().getSessionKey() + "\n" +
-            "Session no  + " + (request.session().set("i", request.session().get("i", 0) + 1)));
-    }
+    //     request.Write("\n");
+    //     request.Send("Session Id = " + request.session().getSessionKey() + "\n" +
+    //         "Session no  + " + (request.session().set("i", request.session().get("i", 0) + 1)));
+    // }
 }
